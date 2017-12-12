18387d0f075296d7636f918e0ccb92fec8a47c67
Webmaster444 <jlmobile710@gmail.com>
2017-12-12 11:43:00 +0100

    layer on/off functionality completed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,38 +1,54 @@
-// var borderRadius = '3px';
-// //Append svg
-// var svg = d3.select("#svg_wrapper").append("svg")
-//     .attr('viewBox', '0 0 1000 500')
-//     .append("g")
-//     .attr("transform", "translate(30,10)");
+$(function(){
+    $('#picBtn').change(function() {
+        var clicked = $(this).is(":checked");
+        if(clicked){
+            $('#imgs_wrapper').css('opacity',0);
+        }else{
+            $('#imgs_wrapper').css('opacity',1);
+        }
+    });
 
-// var width = 960, height = 480;
+    $('#linesBtn').change(function() {
+        var clicked = $(this).is(":checked");
+        if(clicked){
+            $('#line_img').css('opacity',0);
+        }else{
+            $('#line_img').css('opacity',1);
+        }
+    });
 
-// var tenthW = width / 10;
-// var fifthW = width / 5;
-// var tenthH = height / 10;
-// var fifthH = height / 5;
-// //Wrapper Rect
-// var rect = svg.append("rect")    
-//     .attr("x", 0)
-//     .attr("y", 0)
-//     .attr("width", width)
-//     .attr("height", height)
-//     .attr('stroke', '#000');
+    $('#redBtn').change(function() {
+        var clicked = $(this).is(":checked");
+        if(clicked){
+            $('#red_img').css('opacity',0);
+        }else{
+            $('#red_img').css('opacity',1);
+        }
+    });
 
+    $('#blueBtn').change(function() {
+        var clicked = $(this).is(":checked");
+        if(clicked){
+            $('#blue_img').css('opacity',0);
+        }else{
+            $('#blue_img').css('opacity',1);
+        }
+    });        
+    $('#blackBtn').change(function() {
+        var clicked = $(this).is(":checked");
+        if(clicked){
+            $('#black_img').css('opacity',0);
+        }else{
+            $('#black_img').css('opacity',1);
+        }
+    }); 
 
-//     // svg.append('rect').attr('x',3).attr('y',3).attr('width',tenthW).attr('height',tenthH).attr('stroke','#000');
-// var rect1 = drawRect(0,0,tenthW,tenthH,'white');
-// var rect1 = drawRect(tenthW,0,tenthW,tenthH,'white');
-// var rect1 = drawRect(2*tenthW,0,tenthW,tenthH,'white');
-// var rect1 = drawRect(3*tenthW,0,tenthW,tenthH,'white');
-// var rect1 = drawRect(4*tenthW,0,tenthW,tenthH,'white');
-// var rect1 = drawRect(5*tenthW,0,tenthW,tenthH,'white');
-// var rect1 = drawRect(6*tenthW,0,tenthW,tenthH,'white');
-// var rect1 = drawRect(7*tenthW,0,tenthW,tenthH,'white');
-// var rect1 = drawRect(8*tenthW,0,tenthW,tenthH,'white');
-// var rect1 = drawRect(9*tenthW,0,tenthW,tenthH,'white');
-
-// function drawRect(x,y,width,height,color){
-
-//     return svg.append('rect').attr('x',x+3).attr('y',y+3).attr('width',width).attr('height',height).attr('stroke','#000').style('fill',color).attr('class',color+'_rect');    
-// };
+    $('#yellowBtn').change(function() {
+        var clicked = $(this).is(":checked");
+        if(clicked){
+            $('#yellow_img').css('opacity',0);
+        }else{
+            $('#yellow_img').css('opacity',1);
+        }
+    }); 
+})
