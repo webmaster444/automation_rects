2d00a310273eb8dba1883083d8af60a52809d8eb
Webmaster444 <jlmobile710@gmail.com>
2017-12-12 11:11:47 +0100

    resources added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,23 +1,38 @@
-var borderRadius = '3px';
-//Append svg
-var svg = d3.select("#svg_wrapper").append("svg")
-    .attr('viewBox', '0 0 1000 500')
-    .append("g")
-    .attr("transform", "translate(30,10)");
+// var borderRadius = '3px';
+// //Append svg
+// var svg = d3.select("#svg_wrapper").append("svg")
+//     .attr('viewBox', '0 0 1000 500')
+//     .append("g")
+//     .attr("transform", "translate(30,10)");
 
-var width = 960, height = 480;
+// var width = 960, height = 480;
 
-var tenthW = width / 10;
-var fifthW = width / 5;
-var tenthH = height / 10;
-var fifthH = height / 5;
-//Wrapper Rect
-var rect = svg.append("rect")    
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("width", width)
-    .attr("height", height)
-    .attr('stroke', '#000');
+// var tenthW = width / 10;
+// var fifthW = width / 5;
+// var tenthH = height / 10;
+// var fifthH = height / 5;
+// //Wrapper Rect
+// var rect = svg.append("rect")    
+//     .attr("x", 0)
+//     .attr("y", 0)
+//     .attr("width", width)
+//     .attr("height", height)
+//     .attr('stroke', '#000');
 
 
-    svg.append('rect').attr('x',3).attr('y',3).attr('width',tenthW).attr('height',tenthH).attr('stroke','#000');
+//     // svg.append('rect').attr('x',3).attr('y',3).attr('width',tenthW).attr('height',tenthH).attr('stroke','#000');
+// var rect1 = drawRect(0,0,tenthW,tenthH,'white');
+// var rect1 = drawRect(tenthW,0,tenthW,tenthH,'white');
+// var rect1 = drawRect(2*tenthW,0,tenthW,tenthH,'white');
+// var rect1 = drawRect(3*tenthW,0,tenthW,tenthH,'white');
+// var rect1 = drawRect(4*tenthW,0,tenthW,tenthH,'white');
+// var rect1 = drawRect(5*tenthW,0,tenthW,tenthH,'white');
+// var rect1 = drawRect(6*tenthW,0,tenthW,tenthH,'white');
+// var rect1 = drawRect(7*tenthW,0,tenthW,tenthH,'white');
+// var rect1 = drawRect(8*tenthW,0,tenthW,tenthH,'white');
+// var rect1 = drawRect(9*tenthW,0,tenthW,tenthH,'white');
+
+// function drawRect(x,y,width,height,color){
+
+//     return svg.append('rect').attr('x',x+3).attr('y',y+3).attr('width',width).attr('height',height).attr('stroke','#000').style('fill',color).attr('class',color+'_rect');    
+// };
